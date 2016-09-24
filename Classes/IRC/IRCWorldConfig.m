5d0da1858fe3a99f23b972840130321d702cdca6
Denis Denisov <denji0k@gmail.com>
2016-09-25 02:33:32 +0300

    GPLv2 Headers

--- Classes/IRC/IRCWorldConfig.m
+++ Classes/IRC/IRCWorldConfig.m
@@ -1,5 +1,17 @@
-// LimeChat is copyrighted free software by Satoshi Nakagawa <psychs AT limechat DOT net>.
-// You can redistribute it and/or modify it under the terms of the GPL version 2 (see the file GPL.txt).
+// Copyright (C) 2007-2014 Satoshi Nakagawa <psychs AT limechat DOT net>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #import "IRCWorldConfig.h"
 #import "IRCClientConfig.h"
